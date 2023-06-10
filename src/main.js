cb2ba994cc387af2ad2add7f7d3b1f1ce2046920
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-10 11:34:37 +0530

    basic explanation

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
-import { insertMessage, readMessage } from "./mydb.js";
+import { MongoClient } from "mongodb";
 
 async function main() {
-  // insert
-  let jsonDocument = {
-    message: "Hello Universee",
-    to: "santosh",
-    from: "tejas",
-  };
-  await insertMessage(jsonDocument);
+  const uri = "mongodb://127.0.0.1:27017";
+  // open the connection
+  const client = new MongoClient(uri);
 
-  // read
-  let list = await readMessage();
-  console.log(list);
+  const db = client.db("mydb");
+  const messageCollection = db.collection("message");
+
+ let inputDoc = {message : "Hello"};
+  await messageCollection.insertOne(inputDoc);
+  console.log("Record Added Successfully");
+
+  // close the connection
+  await client.close();
+  
 }
 
 main();

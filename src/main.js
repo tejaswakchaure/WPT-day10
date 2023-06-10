8472fc6ddddd3685e5280ced6c7729b802c770a4
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-10 10:22:59 +0530

    b3 insert msg

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-//import {} 
+import { MongoClient } from "mongodb";
+
+async function insertRecord(jsonDocument) {
+  const uri = "mongodb://127.0.0.1:27017";
+  const client = new MongoClient(uri);
+
+  const db = client.db("mydb");
+  const message = db.collection("message");
+
+  await message.insertOne(jsonDocument);
+
+  await client.close();
+  console.log("Record Added");
+}
+
+async function main() {
+  let jsonDocument = {
+    message: "Hello Universee",
+    to: "santosh",
+    from: "tejas",
+  };
+  await insertRecord(jsonDocument);
+}
+
+main();

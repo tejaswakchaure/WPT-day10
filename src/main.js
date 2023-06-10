bd9296bfb00f5a0329db0865999eab8372ce6d6c
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-10 11:36:56 +0530

    bsic

--- src/main.js
+++ src/main.js
@@ -1,20 +1,7 @@
 import { MongoClient } from "mongodb";
 
 async function main() {
-  const uri = "mongodb://127.0.0.1:27017";
-  // open the connection
-  const client = new MongoClient(uri);
-
-  const db = client.db("mydb");
-  const messageCollection = db.collection("message");
-
- let inputDoc = {message : "Hello"};
-  await messageCollection.insertOne(inputDoc);
-  console.log("Record Added Successfully");
-
-  // close the connection
-  await client.close();
-  
+return "hello World"
 }
 
 main();

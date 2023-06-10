1b28a5f4baa68c5f9bef14cdf0562581e8420715
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-10 10:13:52 +0530

    connect mongo

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-//import {} 
+import { MongoClient } from "mongodb";
+
+async function main(){
+    const uri = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
+
+    const db = client.db("mydb");
+
+    const message = db.collection("message");
+    
+
+    const jsonDocument = await message.insertOne({"username":"tejas"});
+    await client.close();
+    console.log("Record Added...")
+    
+}
+
+main();
